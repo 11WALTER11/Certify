444270b5f81ee48dfe0efb09e67dc54f8a9f9625
11WALTER11 <siddarish.iul.ac.in>
2022-12-04 23:16:24 +0530

    udpated

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import "./App.css";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import jsPDF from "jspdf"
-import Certificate from '../src/certificate/Certificate.png'
+import ACertificate from '../src/certificate/appreciationC.png'
 // eslint-disable-next-line
-import CertificateS from '../src/certificate/Certificates.png'// eslint-disable-next-line
+import PCertificate from '../src/certificate/participationC.png'// eslint-disable-next-line
 import { useState } from 'react';
 
 
@@ -13,6 +13,8 @@ const events = ["Skit" , "Singing Competition" , "Feel the Reel","xquizite","Ibm
 " Feel The Reel"];
 function App() {
   const [sName, setsName] = useState("");
+  const[YearB, setYearB] = useState("")
+  const[position, setPosition] = useState("");
   const [sEname, setEname] = useState("");
   const [Cname, setCname] = useState("");
   const [Ed, setEd] = useState("");
@@ -20,11 +22,18 @@ function App() {
  const pdfGenerator=()=>{
   
        var doc= new jsPDF('landscape','px','a4','false');
-       doc.addImage(Certificate,'PNG',65,20,500,400);
-           doc.text(290,200,sName);       // Student Name
-           doc.text(297,235,sEname) // Event name
-           doc.text(140,256,Cname)  // Club name
-           doc.text(290,275,Ed) // event Date
+       doc.addImage(ACertificate,'PNG',65,20,500,400);
+           doc.setFontSize(15);
+           doc.setFont('courier', 'bolditalic');
+           doc.text(238,200,sName,);  // Student Name
+           
+           doc.setFontSize(13);
+           doc.setFont('time','italic',);
+           doc.text(122,232,YearB);    // Year branch
+           doc.text(233,232,position); // position
+           doc.text(358,232,sEname); // Event name
+           doc.text(173,247,Cname);  // Club name
+           doc.text(290,261,Ed); // event Date
        doc.save(`${sName}'s_${sEname}certificate.pdf`);
   }
 
@@ -38,10 +47,14 @@ function App() {
           <label for="fname">Student Name:</label>
           <input type="text" id="fname" name="fname" onChange={(e)=>setsName(e.target.value)} />
           <br />
+          <label for="YB">Year/branch:</label>
+          <input type="text" id="YB" name="YB" onChange={(e)=>setYearB(e.target.value)} />
           <br />
-          <label for="YearB">Enter Year / Branch :</label>
-          <input type="text" id="YearB" name="YearB" />
+          <label for="POS">Position:</label>
+          <input type="text" id="POS" name="POS" onChange={(e)=>setPosition(e.target.value)} />
           <br />
+          
+          
           <br />
           <label for="ClubN">Enter Club Name :</label>
           <input type="text" id="ClubN" name="ClubN" onChange={(e)=>setCname(e.target.value)}/>
